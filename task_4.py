4888d6c7f16bf02b132afa5b04505e705dc39ee0
SvitlanaSavchenko <134533907+SvitlanaSavchenko@users.noreply.github.com>
2024-08-03 19:57:36 +0200

    task_4-done

---
+++ task_4.py
@@ -0,0 +1,88 @@
+import uuid
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+def visualize_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]["color"] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]["label"] for node in tree.nodes(data=True)}
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+def build_heap(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def convert_array_to_heap_tree(arr):
+    if not arr:
+        return None
+
+    build_heap(arr)
+
+    nodes = [Node(arr[i]) for i in range(len(arr))]
+    for i in range(len(arr) // 2):
+        if 2 * i + 1 < len(arr):
+            nodes[i].left = nodes[2 * i + 1]
+        if 2 * i + 2 < len(arr):
+            nodes[i].right = nodes[2 * i + 2]
+
+    return nodes[0]  # корінь дерева
+
+def main():
+    # Створення дерева
+    root = Node(0)
+    root.left = Node(4)
+    root.left.left = Node(5)
+    root.left.right = Node(10)
+    root.right = Node(1)
+    root.right.left = Node(3)
+    
+    # Візуалізація дерева
+    visualize_tree(root)
+
+if __name__ == "__main__":
+    main()
